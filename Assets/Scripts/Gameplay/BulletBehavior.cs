6b977a698fc123666a8846609b8e1cb099c8e445
Cade Gordon <cadegordon093@gmail.com>
2022-04-04 12:50:15 -0500

    made bullets and turret behaviors enemies now die

--- Assets/Scripts/Gameplay/BulletBehavior.cs
+++ Assets/Scripts/Gameplay/BulletBehavior.cs
@@ -7,11 +7,27 @@ public class BulletBehavior : MonoBehaviour
     private string _ownerTag;
     [SerializeField]
     private float _damage;
+    [SerializeField]
+    private float _lifeTime;
+    [SerializeField]
+    private bool _destroyOnHit;
+    private float _currentLifeTime;
+    private Rigidbody _rigidBody;
 
     public string OwnerTag
     {
         get { return _ownerTag; }
         set { _ownerTag = value; }
+    }
+
+    public Rigidbody Rigidbody
+    {
+        get { return _rigidBody; }
+    }
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,5 +41,16 @@ public class BulletBehavior : MonoBehaviour
             return;
 
         otherHeatlh.TakeDamage(_damage);
+
+        if (_destroyOnHit)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        _currentLifeTime += Time.deltaTime;
+
+        if (_currentLifeTime >= _lifeTime)
+            Destroy(gameObject);
     }
 }
